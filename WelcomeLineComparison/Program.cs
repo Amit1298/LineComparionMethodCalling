6391264521ca517705e3700166243eb4aed67658
Amit <amitkr24031995@gmail.com>
2021-09-27 17:26:13 +0530

    Adding welcome Message

---
+++ WelcomeLineComparison/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WelcomeLineComparison
+{
+    class Program
+    {
+        public static void lineCom()
+        {
+            Console.WriteLine("Welcome to Line Comparison");
+        }
+        static void Main(string[] args)
+        {
+            Program.lineCom();
+        }
+    }
+}
