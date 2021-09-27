5c1ff10db627ece39888cf6351b267256633fe22
Amit <amitkr24031995@gmail.com>
2021-09-27 18:17:11 +0530

    Adding UC1Cartesian system

---
+++ WelcomeLineComparison/UC1Cartesian system,.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cartesian_system_
+{
+    class Program
+    {
+        public void cartesian()
+        {
+            int lineLength;
+            Console.Write("Length of x1 : ");
+            int linex1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length of x2 : ");
+            int linex2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length of y1 : ");
+            int liney1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length of y2 : ");
+            int liney2 = Convert.ToInt32(Console.ReadLine());
+            lineLength = (linex2 - linex1) * (linex2 - linex1) - (liney2 - liney1) * (liney2 - liney1);
+            Console.WriteLine("Length of two points : " + Math.Sqrt(lineLength));
+        }
+        static void Main(string[] args)
+        {
+            Program CartSys = new Program();
+            CartSys.cartesian();
+        }
+    }
+}
